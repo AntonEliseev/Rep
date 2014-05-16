4a7e0bac0697f3240d1fa2b443c5307fdd7b70d5
Михаил <bezrukv.misha@rambler.ru>
2014-05-16 14:23:28 +0400

    Rep

---
+++ Новая папка (3)/ConsoleApplication12/ConsoleApplication12/Classtree.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication12
+{
+    class Tree
+    {
+        public class Node 
+        {
+            public int data;
+            public Node parent;
+            public Node left;
+            public Node right; 
+            public Node(int data,Node parent, Node left, Node right)
+            {
+                this.data = data;
+                this.parent = parent;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        private Node root;
+        public Tree()
+        {
+            root = null;
+        }
+
+        //Вставка в дерево
+        public void Insert(int data)
+        {
+            if (root == null)     
+                root = new Node(data,null, null, null);
+            else
+            {
+                Node p = root;
+                bool ok = false;
+                while (!ok)
+                {
+                    if (data > p.data)
+                        if (p.right == null)
+                        {
+                            p.right = new Node(data, p, null, null);
+                            ok = true;
+                        }
+                        else p = p.right;
+                    else
+                        if (data < p.data)
+                            if (p.left == null)
+                            {
+                                p.left = new Node(data, p, null, null);
+                                ok = true;
+                            }
+                            else p = p.left;
+                        else
+                            ok = true;
+                }
+            }
+        }
+
+        //Печать дерева
+        private void WriteNode(Node p, StreamWriter w)
+        {
+            if (p != null)
+            {
+               if (p.left != null)
+                {
+                    w.WriteLine(Convert.ToString(p.data) + "->" + Convert.ToString(p.left.data) + ";");
+                    WriteNode(p.left, w);
+                }
+                if (p.right != null)
+                {
+                    w.WriteLine(Convert.ToString(p.data) + "->" + Convert.ToString(p.right.data) + ";");
+                    WriteNode(p.right, w);
+                }
+            }
+        }
+        public void WriteTree(string path)
+        {
+            FileStream f = new FileStream(path, FileMode.Create);
+            StreamWriter w = new StreamWriter(f);
+            w.WriteLine("digraph G {");
+            WriteNode(root, w);
+            w.WriteLine("}");
+            w.Close();
+            f.Close();
+        }
+        public void WriteTree()
+        {
+            WriteTree("graph.gv");
+        }
+
+        //Удаление элемента из дерева
+        Node q = new Node(0, null, null, null);
+        private void Delete(ref Node r)
+        {
+            if (r.right != null)
+                Delete(ref r.right);
+            else
+            {
+                q.data = r.data;
+                q = r;
+                r = r.left;
+            }
+        }
+        private void DeleteNode(int data, ref Node p) //удаление узла
+        {
+            if (p != null)
+                if (data < p.data)
+                    DeleteNode(data, ref p.left);
+                else
+                    if (data > p.data)
+                        DeleteNode(data, ref p.right);
+                    else
+                    {
+                        q = p;
+                        if (q.right == null)
+                            p = q.left;
+                        else
+                            if (q.left == null)
+                                p = q.right;
+                            else
+                                Delete(ref q.left);
+                    }
+        }
+        public void Remove(int data)
+        {
+            DeleteNode(data, ref root);
+        }
+
+        //Поиск элемента в дереве
+        public Node Find(int data)
+        {
+            if (root != null)
+                return FindNode(root, data);
+            else
+                return null;
+        }
+        private Node FindNode(Node p, int data)
+        {
+            
+            {
+                Node result = null;
+                if (p.left != null)
+                    result = FindNode(p.left, data);
+                if ((result == null) && (p.right != null))
+                    result = FindNode(p.right, data);
+                return result;
+            }
+        }
+
+    
+        
+        //Количество узлов на k-м уровне дерева.
+        private int Count(Node r, int k)
+        {
+            if (k == 1)
+                if (r != null)
+                    return 1;
+                else
+                    return 0;
+            else
+                if (r != null)
+                    return Count(r.left, k - 1) + Count(r.right, k - 1);
+            return 0;
+        }
+        public int CountOnLevel(int k)
+        {
+            return Count(root, k);
+        }
+    }
+}
---
+++ Новая папка (3)/ConsoleApplication12/ConsoleApplication12/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree t = new Tree();
+            Random rnd = new Random();
+            for (int i = 0; i < 30; i++)
+                t.Insert(rnd.Next(50));
+            int k = t.CountOnLevel(3);
+            Console.WriteLine(k);
+            t.WriteTree("OutPut.txt");
+            Console.ReadKey();
+            
+        }
+    }
+}
